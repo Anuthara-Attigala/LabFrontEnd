5139e4c3795647b32494a03366a720ab836954e1
Surandhi Fernando <surandhi.fdo94@gmail.com>
2017-07-01 19:33:58 +0530

    commit 5

--- public/patient.service.js
+++ public/patient.service.js
@@ -2,7 +2,7 @@ angular.module("labApp").factory("PatientService",['$http',function ($http) {
     return{
         get:()=>$http.get('http://localhost:3000/patient').then(response=>response.data),
         deletepatient:id=>$http.delete('http://localhost:3000/patient/'+id).then(response=>response.data),
-        updatepatient:(patient)=>$http.put('http://localhost:3000/patient/',patient).then(response=>response.data)
+        updatepatient:id=>$http.put('http://localhost:3000/patient/',+id).then(response=>response.data)
         // getLabTests:()=>$http.get('/labTests').then(response=>response.data)
     };
 }]);
