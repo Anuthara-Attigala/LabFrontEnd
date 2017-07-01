06c46e2a6c9c2d4af7dc5f1a8aa6964e90745931
Surandhi Fernando <surandhi.fdo94@gmail.com>
2017-07-01 15:07:03 +0530

    commit

--- public/patientManagement.service.js
+++ public/patientManagement.service.js
@@ -1,7 +1,7 @@
 angular.module("labApp").factory("PatientManagementService",['$http',function ($http) {
     return{
         get:()=>$http.get('http://localhost:3000/patient').then(response=>response.data),
-        add: patient=>$http.post('http://localhost:3000/patient',patient).then(response =>response.data)
+        add: patient=>$http.post('http://localhost:3000/patient',patient).then(response =>response.data),
         updatepatient:(patients)=>$http.put('http://localhost:3000/patient'+patient).then(response=>response.data)
     };
 }]);
