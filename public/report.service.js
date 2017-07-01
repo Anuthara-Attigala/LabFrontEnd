bd3bf0f0b011e9620d4b4ca94e0ffdd436d9b2de
RajithaMendis <rajitha23mendis@gmail.com>
2017-07-01 18:04:30 +0530

    added pharmacy controller

--- public/report.service.js
+++ public/report.service.js
@@ -1,7 +1,7 @@
 /**
  * Created by User on 7/1/2017.
  */
-angular.module("myApp").factory("reportService",['$http',function ($http) {
+angular.module("labApp").factory("reportService",['$http',function ($http) {
     return{
         getById:id=>$http.get('http://localhost:3000/requests/'+id).then(response=>response.data),
         update:request=>$http.put('http://localhost:3000/requests/'+request._id,request).then(response=>response.data),
