889e2b8440807fd7f1bf51d0166c437af672d964
RajithaMendis <rajitha23mendis@gmail.com>
2017-07-01 12:58:58 +0530

    changed main.routes.js & test.service

--- public/test.service.js
+++ public/test.service.js
@@ -4,14 +4,14 @@
 angular.module('labApp').factory('testService',['$http',
     function ($http) {
         return{
-            get:()=>$http.get('/requests').then(response=>response.data),
-            update:request=>$http.put('/requests/'+request._id,request).then(response=>response.data),
+            get:()=>$http.get('http://localhost:3000/requests').then(response=>response.data),
+            update:request=>$http.put('http://localhost:3000/requests/'+request._id,request).then(response=>response.data),
 
-                getById:id=>$http.get('/requests/'+id).then(response=>response.data),
+                getById:id=>$http.get('http://localhost:3000/requests/'+id).then(response=>response.data),
 
 
-            add:detail=>$http.post('/addDetails',detail).then(response=>response.data),
-            getLocation:()=>$http.get('/samplecenters').then(response=>response.data)
+            add:detail=>$http.post('http://localhost:3000/addDetails',detail).then(response=>response.data),
+            getLocation:()=>$http.get('http://localhost:3000/samplecenters').then(response=>response.data)
             //       add:request=>$http.post('/requests',request).then(response=>response.data),
             //  update :(id,request)=>$http.put('/requests/'+id,request).then(response=>response.data)
             // update:request=>$http.put('/requests/'+request._id,request.action,request.status).then(response=>response.data)
