04f7ec2a0b2161a5ea6d610495bd3920e37dd786
RajithaMendis <rajitha23mendis@gmail.com>
2017-07-01 12:48:42 +0530

    changed main.routes.js

--- public/main.routes.js
+++ public/main.routes.js
@@ -30,7 +30,6 @@ angular.module("labApp").config(['$routeProvider','$locationProvider',
             templateUrl : 'test2.html',
             controller : 'testController'
 
-        
         }).when('/sample_collected/:requestID',{
             templateUrl : 'result.html'
 
