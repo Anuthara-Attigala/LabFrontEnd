b47958da4d1f90b5df97c75854bae98022f847d4
RajithaMendis <rajitha23mendis@gmail.com>
2017-07-01 16:03:57 +0530

    deleted

--- public/main.routes.js
+++ public/main.routes.js
@@ -3,7 +3,7 @@ angular.module("labApp").config(['$routeProvider','$locationProvider',
         $routeProvider.when('/samplecentertypes', {
             templateUrl: 'samplecentertype.html',
             controller: 'SampleCenterTypeController'
-        }).when('/labtests',{
+        }).when('/test',{
             templateUrl:"test.html",
             controller:"LabTestController"
         }).when('/labtestview',{
@@ -19,33 +19,6 @@ angular.module("labApp").config(['$routeProvider','$locationProvider',
             templateUrl:"department.html",
             controller:"DepartmentController"
 
-        }).when('/pharmacy', {
-            templateUrl: 'pharmacy.html',
-            controller: 'pharmacyController'
-        }).when('/test',{
-            templateUrl : 'labtest.html',
-            controller : 'testController'
-        }).when('/testresult/:testName/patient/:patientName/new/:requestID',{
-            templateUrl : 'test2.html',
-            controller : 'testController'
-        }).when('/testresult/:testName/patient/:patientName/sample_collected/:requestID',{
-            templateUrl : 'result.html',
-            controller :'ResultController'
-        }).when('/testresult/:testName/patient/:patientName/report_generated/:requestID', {
-            templateUrl: 'report.html',
-            controller : 'reportController'
-        }).when('/sample_collected/:requestID', {
-            templateUrl: 'result.html'
-        }).when('/patientmanagement',{
-            templateUrl:"newPatients.html",
-            controller:"PatientManagementController"
-        }).when('/newrequests',{
-            templateUrl:"patient.html",
-            controller:"PatientController"
-        }).when('/newdocRequest',{
-            templateUrl:"newdocRequest.html",
-            controller:"DocRequestController"
         });
-
         $locationProvider.html5Mode(true);
     }]);
